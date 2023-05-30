a97f994742bf778e8d7cfd4f2fff9fd5e965c50c
Aktan Sanhal <aktansanhal@gmail.com>
2023-05-30 16:21:33 +0300

    finished

---
+++ homeworkweek2/src/main/java/com/aktansanhal/homeworkweek2/controller/contract/ProductControllerContract.java
@@ -0,0 +1,19 @@
+package com.aktansanhal.homeworkweek2.controller.contract;
+
+import com.aktansanhal.homeworkweek2.dto.request.ProductRequestDTO;
+import com.aktansanhal.homeworkweek2.dto.response.ProductResponseDTO;
+
+import java.util.List;
+
+public interface ProductControllerContract {
+
+    ProductResponseDTO saveProduct(ProductRequestDTO productRequestDTO);
+
+    List<ProductResponseDTO> getProducts();
+
+    void deleteProduct(Long id);
+
+    ProductResponseDTO updateProduct(Long id, ProductRequestDTO productRequestDTO);
+
+    ProductResponseDTO getProductById(Long id);
+}
---
+++ homeworkweek2/src/main/java/com/aktansanhal/homeworkweek2/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.aktansanhal.homeworkweek2.repository;
+
+
+import com.aktansanhal.homeworkweek2.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+
+    List<Comment> findAllByUserId(Long userId);
+
+    List<Comment> findAllByProductId(Long productId);
+}
